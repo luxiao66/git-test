3bfd6abcb2a739474f74e9c5c3c24f4ad86622ef
luxiao <1251287437@qq.com>
2019-06-10 09:21:49 +0800

    修改

--- git_test_project/src/com/itheima/git/FirstClass.java
+++ git_test_project/src/com/itheima/git/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
 
-
-
+    }
+    public void test(){
+        int i=1;
+        i+=10;
     }
 }
